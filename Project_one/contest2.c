47a28f710df6cdce36ef581d74fdc3e94df79895
KonuhovAND <andreykonuhov8@gmail.com>
2025-09-14 15:09:07 +0300

    redo and tried to compile and run code)

--- Project_one/contest2.c
+++ Project_one/contest2.c
@@ -1,11 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <locale.h>
-int not_main1()
+int main()
 {
 	int a;
 	setlocale(0, "");
 	scanf("%d", &a);
-	printf("%d inch = %.2f см", a, a * 2.54);
+	printf("%d inch = %.2f пїЅпїЅ", a, a * 2.54);
 	return 0;
 }
