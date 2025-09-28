b855e08bfe6795ac53a0b0c6ba2362de0af10de9
KonuhovAND <andreykonuhov8@gmail.com>
2025-09-28 13:51:44 +0300

    new new

--- Project_one/contest2_ya/task3.c
+++ Project_one/contest2_ya/task3.c
@@ -13,19 +13,20 @@ int main() {
         return 0;
     }
 
+    if (cows <= 0 || cows >= 100) {
+        printf("ошибка ввода данных");
+        return 0;
+    }
 
     int last_digit = cows % 10;
     int last_two = cows % 100;
 
     if (last_digit == 1 && last_two != 11) {
         printf("На лугу пасется %d корова.", cows);
-    	return 0;
     } else if (last_digit >= 2 && last_digit <= 4 && !(last_two >= 12 && last_two <= 14)) {
         printf("На лугу пасется %d коровы.", cows);
-    	return 0;
     } else {
         printf("На лугу пасется %d коров.", cows);
-    	return 0;
     }
 
     return 0;
