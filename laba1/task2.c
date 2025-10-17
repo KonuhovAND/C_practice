ae290d57abbcfc39f712ea1b59af2d9859a867ba
KonuhovAND <andreykonuhov8@gmail.com>
2025-10-17 11:03:45 +0300

    new new

--- laba1/task2.c
+++ laba1/task2.c
@@ -7,7 +7,7 @@ int main() {
     int N;
     int fool = scanf("%d", &N);
 
-    int *seq; // максимум 100 элементов для простоты
+    int *seq;
     if (fool != 1 || N < 1 || N > 100) {
         printf("Ошибка ввода\n");
         return 0;
@@ -27,24 +27,34 @@ int main() {
         }
     }
 
-
     int i1 = -1, i2 = -1;
+    
+    
     for (int i = 0; i < N; i++) {
-        if (seq[i] >0 && i1 == -1) {
+        if (seq[i] > 0) {
             i1 = i;
-        } else if (seq[i] >i && i1 != -1 && i2 == -1) {
-            i2 = i;
-            break;
+            break;  
         }
     }
-    int sum;
-    sum = 0;
-    if (i1 != -1 && i2 != -1) {
+    
+    
+    for (int i = 0; i < N; i++) {
+        if (seq[i] > i) {
+            i2 = i;  
+        }
+    }
+    
+    int sum = 0;
+    int count = 0;
+    
+    if (i1 != -1 && i2 != -1 && i1 < i2) {
         for (int i = i1 + 1; i < i2; i++) {
             sum += seq[i];
+            count++;
         }
     }
+    
     free(seq);
-    printf("%d %d\n", sum, i2-i1-1);
+    printf("%d %d\n", sum, count);
     return 0;
 }
